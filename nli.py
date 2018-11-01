ad606998cd613918d0b3abace5b60355010d7de8
Brian Falkenstein <bkf15@pitt.edu>
2018-11-01 11:55:01 -0400

    added mega documents

--- nli.py
+++ nli.py
@@ -10,31 +10,35 @@ from nltk import bigrams
 
 csv = open('TOEFL11-TRAIN/data/text/index-training.csv')
 
+#md = array of all the mega docs
+#langs contains the languages. lang[i] = the lanuage of mds[i]
+langs = []
+mds = []
+
 #each line in csv corresponds to a different piece of training data, IE an essay
 for line in csv:
 	#splits each line into array of length 4
-	# file[0] = file_name
-	# file[1] = prompt
-	# file[2] = native language
-	# file[3] = score, although I don't think we'll even need this
+	# response_info[0] = file_name
+	# response_info[1] = prompt
+	# response_info[2] = native language
+	# response_info[3] = score, although I don't think we'll even need this
 	response_info = re.split(',', line)
+
+	if response_info[2] not in langs:
+		langs.append(response_info[2])
+
 	#define path name for training file 
 	response_path_name = 'TOEFL11-TRAIN/data/text/responses/tokenized/' + response_info[0]
 	#let response be the file with the essay in it
-	# go through the response file, add its data to our models
+	# go through the response file, add its data to MD
 	with open(response_path_name) as response:
-		#get word n-gram data?
-		#get character n-gram data?
-		#get POS data?
-		#add data to our models
+		#add essay to appropriate MD
+		if len(mds) < langs.index(response_info[2]) + 1:
+			mds.append(response.read().replace('\n', ' '))
+		else:
+			mds[langs.index(response_info[2])] += response.read().replace('\n', '')
 
-		#note that because we are using the tokenized responses, we do not need to worry about separating punctuation 
-		# from the words. Will probably just choose to ignore punctuation, but we'll see 
 
-		for l in response:
-			file_bigram = list(bigrams(l.split()))
-			print(file_bigram)
-		break
 
 
 

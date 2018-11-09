539b0583714408813a31b4fc5f06bf4bfcdd7771
Brian Falkenstein <bkf15@pitt.edu>
2018-11-09 12:43:31 -0500

    unique words and chars added

--- nli.py
+++ nli.py
@@ -38,16 +38,61 @@ def setupTrainingData():
 	md = []
 	for i in range(0, len(langs)):
 		md.append((langs[i], mds[i]))
+	csv.close()
 	return md
 
+#writes to a file called unique_words.txt
+# writes out all words that occur in the corpus. Words that appear many times in the corpus only
+# appear once in the file. 
+def getUniqueWords():
+	csv = open('TOEFL11-TRAIN/data/text/index-training.csv')
+	words = []
+	for line_in_list in csv:
+		response_info = re.split(',', line_in_list)
+		response_path_name = 'TOEFL11-TRAIN/data/text/responses/tokenized/' + response_info[0]
+		with open(response_path_name) as response:
+			for line_in_doc in response:
+				word_list = re.sub("[^\w]", " ",  line_in_doc).split()
+				for word in word_list:
+					if word not in words:
+						words.append(word)
+	f = open('unique_words.txt', 'w')
+	for word in words:
+		f.write(word + '\n')
+	f.close
+
+#writes out to a file called unique_chars.txt that will contain all the chars that appear in corpus
+def getUniqueChars():
+	csv = open('TOEFL11-TRAIN/data/text/index-training.csv')
+	chars = []
+	for line_in_list in csv:
+		response_info = re.split(',', line_in_list)
+		response_path_name = 'TOEFL11-TRAIN/data/text/responses/tokenized/' + response_info[0]
+		with open(response_path_name) as response:
+			for line_in_doc in response:
+				for char in line_in_doc:
+					if char not in chars:
+						chars.append(char)
+	f = open('unique_chars.txt', 'w')
+	for char in chars:
+		f.write(char + '\n')
+	f.close()
+
+getUniqueWords()
+
+#md = setupTrainingData()
+#print(md[3][0])
+
+#want: words on different lines, characters separated by spaces
+# document indexed by words (doc[i] = word_i)
+#   periods as their own words 
+# mega document per language, on each line specify:
+#    documentindex | wordindex | actualword | POS 
+# find max sentence length !!!!
+# 
 
 
-md = setupTrainingData()
-
-
-
-
-
+#write a random baseline, randomly select NL
 
 
 

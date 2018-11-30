ae1187c96e4ac68833a5ef6f72dc0daaddc008f0
Anthony Sicilia <abs92@fq-wifi-10-215-181-31.wireless.pitt.edu>
2018-11-30 16:09:08 -0500

    Talked to Yanbing, we needed to make it so dev_labels were not needed in evaluation_setup.py

--- evaluation_setup.py
+++ evaluation_setup.py
@@ -7,7 +7,44 @@ import csv
 from tensorflow.keras.backend import manual_variable_initialization 
 import os
 import constants as const
-from model import extract_from, model_input
+
+def extract_from(tokenized_fs, vocab, classes, num_sent_uppr = 36, sent_len_upper = 256):
+	files = [f for f in sorted(os.listdir(tokenized_fs))]
+	max_num_sentences = -1
+	max_sentence_length = -1
+	documents = []
+	slens = []
+	for f in files:
+		sentences = [[vocab(c) for c in s.strip('\n')] for s in list(open(tokenized_fs + f))]
+		max_num_sentences = min(max(len(sentences), max_num_sentences), num_sent_uppr)
+		max_sentence_length = min(max(np.max([len(s) for s in sentences]), max_sentence_length), sent_len_upper)
+		documents += [sentences]
+
+	# USED TO DETERMINE UPPERBOUNDs ON SENTENCE LENGTH AND NUMBER OF SENTENCES FROM TRAINING DATA
+	############################################################################################
+		# slens += [len(s) for s in sentences]
+	#     slens += [len(sentences)]
+	# print(np.mean(slens))
+	# print(np.percentile(slens, 99))
+	# print(max_num_sentences)
+	# print(max_sentence_length)
+	# exit()
+	############################################################################################
+
+	return documents, max_num_sentences, max_sentence_length
+
+def model_input(data, max_num_sentences, max_sentence_length, classes):
+	x = np.zeros((len(data), max_num_sentences, max_sentence_length), dtype = np.int64)
+
+	for i, text in enumerate(data):
+		if len(text) > max_num_sentences:
+			text = text[(len(text) - max_num_sentences):]   # truncate to tail
+		for j, sentence in enumerate(text):
+			if len(sentence) > max_sentence_length:
+				sentence = sentence[(len(sentence) - max_sentence_length):]     #truncate to tail
+			for k, c in enumerate(sentence):
+				x[i,j,k] = c
+	return x
 
 
 #does the testing on the development set, returns an array with the predicted label for each piece of data 
@@ -15,12 +52,12 @@ def get_predicted_labels():
 	#set up all the data. This will require calls to model_input and extract_from
 
 	#call extract_from to get some of the required meta data before we call model.predict 
-	test_data, _ , _= extract_from(const.tokenized_dev, const.dev_labels, const.vocab, const.classes)
+	test_data, _ , _= extract_from(const.tokenized_dev, const.vocab, const.classes)
 	#convert the input to a form the model can understand 
 	#NOTE: max_num_sentences and max_sentences are set to the default max for now
 	max_num_sentences = 36
 	max_sentence_length = 256
-	x_test, y_test = model_input(test_data, max_num_sentences, max_sentence_length, const.classes)
+	x_test = model_input(test_data, max_num_sentences, max_sentence_length, const.classes)
 
     #load the model from memory. Note that this will be the only line that needs to change if we change models 
 	mod = keras.models.load_model(const.model_to_load, custom_objects = {'tf':tf,'const':const})
@@ -45,18 +82,11 @@ def get_predicted_labels():
 
 def main():
 	predicted_labels = get_predicted_labels()
-	correct_predictions = 0
-	with open(const.dev_labels) as f:
-		for i, line in enumerate(f):
-			response_info = re.split(',', line)
-			real_label = const.classes[response_info[2]]
-			if real_label == predicted_labels[i]:
-				correct_predictions += 1
 
 	#export the predictions into a CSV file 
 	with open(const.csv_out_file, mode='w') as pred_file:
 		line_writer = csv.writer(pred_file, delimiter=',')
-		file_names = open(const.dev_labels)
+		file_names = [f for f in sorted(os.listdir(const.tokenized_dev))]
 		for i, line in enumerate(file_names):
 			#response_info[0] is the filename 
 			response_info = re.split(',', line)
